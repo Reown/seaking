0549ce5e339aa78b2305121416dd166cb74a34a1
joey <joey@chow.cc>
2025-02-17 01:57:19 +0800

    test working multi

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,62 @@
-import pokemon from "../pokemon.json";
+import { useState } from "react";
+import pokedex from "../pokedex.json";
 import typechart from "../typechart.json";
 import "./App.css";
-import { SetStateAction, useState } from "react";
+
+//pokedex json type safety
+interface pokedexType {
+  id: number;
+  name: string;
+  type: string[];
+  ability: string[];
+}
+
+interface multiType {
+  [key: string]: number;
+}
 
 function App() {
-  const [id, setId] = useState<number[]>([]);
+  const [found, setFound] = useState<pokedexType[]>([]);
 
-  const update = (e: string) => {
-    setId([]);
+  //search pokedex for names that includes substring
+  const searchAll = (e: string) => {
+    setFound([]);
     if (e.length > 2) {
-      let tempid = [] as number[];
-      pokemon.map((item, key) => {
-        if (item.name.includes(e)) {
-          tempid.push(key);
-        }
-      });
-      setId(tempid);
+      setFound(
+        pokedex.filter((item) =>
+          item.name.toLowerCase().includes(e.toLowerCase())
+        )
+      );
     }
   };
 
-  const test = (iditem: number) => {
-    let tempweak = [] as string[];
-    typechart.map((typeitem) => {
-      if (pokemon[iditem].type[0] === typeitem.type) {
-        tempweak = tempweak.concat(typeitem.weak);
+  //deep copy from json to obtain only weak/strong multi
+  const getMulti = (type: string[]) => {
+    let multi: multiType = JSON.parse(
+      JSON.stringify(typechart.find((def) => def.type === "default")?.multi)
+    );
+
+    type.forEach((type) => {
+      const temp: multiType = JSON.parse(
+        JSON.stringify(typechart.find((item) => item.type === type)?.multi)
+      );
+      Object.keys(temp).forEach((key) => {
+        multi[key] *= temp[key];
+      });
+    });
+
+    Object.keys(multi).forEach((key) => {
+      if (multi[key] === 1.0) {
+        delete multi[key];
       }
     });
-    console.log(tempweak);
-    return <>{tempweak}</>;
+    return multi;
   };
 
-  const btnc = () => {
-    console.log();
+  const render = (found: pokedexType) => {
+    const i = getMulti(found.type);
+    console.log(i);
+    return <></>;
   };
 
   return (
@@ -42,18 +67,17 @@ function App() {
           placeholder="Search"
           className="text"
           onChange={(e) => {
-            update(e.target.value);
+            searchAll(e.target.value);
           }}
         ></textarea>
-        <button onClick={() => btnc()}>test</button>
       </div>
-      {id.map((item, key) => (
+      {found.map((found) => (
         <>
           <hr />
-          <div className="card formblock mx-auto">
+          <div className="card mx-auto">
             <div className="card-body">
-              {pokemon[item].name}
-              {test(item)}
+              {found.name}
+              {render(found)}
             </div>
           </div>
         </>
